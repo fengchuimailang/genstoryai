1de36a5cec76f69bf9c4a92ae80c563b755689c9
fengchuimailang <1050575425@qq.com>
2025-07-08 19:57:03 +0800

    github deploy

--- backend/genstoryai_backend/genstoryai_backend/main.py
+++ backend/genstoryai_backend/genstoryai_backend/main.py
@@ -34,5 +34,5 @@ async def root():
 
 if __name__ == "__main__":
     print_env_vars()
-    port = int(os.getenv("PORT", 5000))
+    port = int(os.getenv("PORT", 8000))
     uvicorn.run(app, host="0.0.0.0", port=port)
